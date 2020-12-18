debf2613bcc80865ea26beff340bf9800204ac64
sadaramdeepthi <sadaramdeepthi@gmail.com>
2020-12-18 13:54:24 +0530

    combination of three commits

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // Squashing Practise
 
 console.log("Hello");
+console.log("World");
+console.log("Deepthi");
+console.log("Boodeda");
+
+// commiting on the main master branch
+console.log("React");
+console.log("Javascript");
+console.log("Redux");
